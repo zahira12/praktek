2fd53787389f8242c7ab6aab8246917293acff88
Zahraalfaiqoh <zahirazahraalfaiqoh28@gmail.com>
2018-10-12 21:05:38 +0700

    perhitungan luas persegi panjang

---
+++ latihan/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int p, l, L,;
+    cout << "masukan panjang =";
+    cin>>p;
+    cout << "masukan lebar =";
+    cin>>l;
+    cout << "luasnya adalah =" <<L<< end ;
+    L= p*l;
+}
