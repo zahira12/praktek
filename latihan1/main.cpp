928339220ac2a69245a9864043cdc7497bbd1471
Zahraalfaiqoh <zahirazahraalfaiqoh28@gmail.com>
2018-10-12 22:23:36 +0700

    mengetahui suatu bilangan genap/ganjil

---
+++ latihan1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y ;
+    cout << "masukan angka :" << endl;
+    cin>>y ;
+        if (x%2= = 0)
+        cout<<" Angka yang dimasukan adalah Genap ";
+        else
+        cout<<" Angka yang dimasukan adalah Ganjil ";
+    return 0;
+}
